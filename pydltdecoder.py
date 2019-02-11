6bdc8a6e4454393adb364cce5bbcba6061acf9f8
Vladimir Shapranov <equidamoid@gmail.com>
2019-02-11 22:43:32 +0100

    quick and dirty dbus decoder

--- pydltdecoder.py
+++ pydltdecoder.py
@@ -11,6 +11,8 @@
 
 
 import logging
+import dltpy.dltfile
+import debus.marshalling
 
 class BaseDecoderPlugin:
     def load_config(self, fn):
@@ -35,32 +37,67 @@ class ReprDecoderPlugin(BaseDecoderPlugin):
         self.cache = {}
 
     def check_message(self, msg):
-        print("Checking message %r" % msg)
+        logging.info("Checking message %r", msg)
         return True
     
     def decode_message(self, msg):
-        print("Decoding message...")
-        logging.warning("pl: %s")
-        return True, repr(msg)
+        logging.warning("app: %r, ctx: %r, ts: %d, payload: %r", msg['app'], msg['ctx'], msg['ts'], msg['pl'])
+        pl = dltpy.dltfile.parse_payload(msg['pl'])
+        return True, repr(pl)
+
+class DBusDecoderPlugin(BaseDecoderPlugin):
+    def __init__(self):
+        self.cache = {}
+
+    def check_message(self, msg):
+        return msg['app'] in ('DBSE', 'DBSY') and msg['ctx'] in ('DIN', 'DOUT')
+    
+    def decode_message(self, msg):
+        #logging.warning("app: %r, ctx: %r, ts: %d, payload: %r", msg['app'], msg['ctx'], msg['ts'], msg['pl'])
+        pl = dltpy.dltfile.parse_payload(msg['pl'])
+        dbus_pl = pl[1]
+        
+        msg = debus.marshalling.read_message(dbus_pl)
+        assert(len(msg) == 1)
+        msg = msg[0]
+        
+        ret = '[%5d:%5s] %s %s %s.%s %r' % (
+            msg.serial,
+            msg.reply_serial if msg.message_type in (debus.MessageType.METHOD_RETURN, debus.MessageType.ERROR) else 'N/A',
+            msg.message_type.name,
+            msg.path,
+            msg.interface,
+            msg.member,
+            msg.payload,
+        )
+        return True, ret
 
 class DecoderMaster(BaseDecoderPlugin):
     def __init__(self):
         self.children = []
 
     def check_message(self, msg):
-        for i in self.children:
-            if i.check_message(msg):
-                return True
+        try:
+            for i in self.children:
+                if i.check_message(msg):
+                    return True
+        except:
+            logging.exception('')
         return False
 
     def decode_message(self, msg):
-        for i in self.children:
-            if i.check_message(msg):
-                return i.decode_message(msg)
+        try:
+            for i in self.children:
+                if i.check_message(msg):
+                    return i.decode_message(msg)
+        except Exception as ex:
+            logging.exception('')
+            return True, str(ex)
         return False, ''
 
 # Create an instance of your decoder and put it to a global variable named "decoder"
 
 decoder = DecoderMaster()
+decoder.children.append(DBusDecoderPlugin())
 decoder.children.append(ReprDecoderPlugin())
 
